6a01fc439e391b20349679a8b09feb7ed4f61b85
pictuga <contact@pictuga.com>
2020-03-20 12:30:42 +0100

    feeds: better handle "empty" datetime

--- morss/feeds.py
+++ morss/feeds.py
@@ -469,7 +469,10 @@ class ParserHTML(ParserXML):
 
 
 def parse_time(value):
-    if isinstance(value, basestring):
+    if value is None or value == 0:
+        return None
+
+    elif isinstance(value, basestring):
         if re.match(r'^[0-9]+$', value):
             return datetime.fromtimestamp(int(value), tz.UTC)
 
@@ -483,7 +486,7 @@ def parse_time(value):
         return value
 
     else:
-        return False
+        return None
 
 
 class ParserJSON(ParserBase):
