70db524664b328066b16f7ecbe92f647c56ffc62
pictuga <contact@pictuga.com>
2021-11-24 21:23:16 +0100

    docker: allow sh run

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
-if [ -z "$1" ] || [ "$@" = "run" ]; then
+if [ "$1" = "sh" ] || [ "$1" = "bash" ]; then
+	exec $@
+
+elif [ -z "$1" ] || [ "$@" = "run" ]; then
 	gunicorn --bind 0.0.0.0:${PORT:-8000} --workers 4 --worker-class=gevent --preload --access-logfile - morss
+
 else
 	morss $@
+
 fi
