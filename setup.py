dfb2b83c067ef63f8dbf51591cbb29e6d150b010
pictuga <contact@pictuga.com>
2023-06-24 01:50:12 +0200

    actions: fix python setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires = ['lxml', 'bs4', 'python-dateutil', 'chardet'],
     extras_require = {
         'full': ['redis', 'diskcache', 'gunicorn', 'setproctitle'],
-        'dev': ['pylint', 'pytest', 'pytest-cov'],
+        'dev': ['pylint', 'pyenchant', 'pytest', 'pytest-cov'],
     },
     python_requires = '>=2.7',
     package_data = {package_name: ['feedify.ini']},
